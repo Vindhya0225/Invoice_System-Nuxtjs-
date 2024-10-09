9685ec22d6198602b4febde222ccd8a4cce3e84e
Vindhya0225 <vindhya0225@gmail.com>
2024-10-10 00:55:36 +0530

    Delete plugins directory

--- plugins/toastification.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
-
-Vue.use(Toast, {
-  position: 'top-right' // Set default position for all toasts
-});
