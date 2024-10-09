2d086066590ca63d71fd6265c7b3b18d14b516ee
Vindhya0225 <vindhya0225@gmail.com>
2024-10-10 00:57:46 +0530

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import navBar from '../components/navBar.vue';
+
+export default {
+  components: {
+    navBar,
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  margin-bottom: 20px;
+}
+
+nav a {
+  margin-right: 10px;
+  text-decoration: none;
+  color: blue;
+}
+</style>
